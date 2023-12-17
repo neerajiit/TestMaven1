1d3630bc203645f56da2dce4c0e0d9dd201ed8ba
Neeraj <neerajiitdh@gmail.com>
2023-12-17 11:22:30 +0530

    Changes on the main class

--- src/main/java/com/neeraj/TestMaven1/TestMaven1Application.java
+++ src/main/java/com/neeraj/TestMaven1/TestMaven1Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestMaven1Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Maven");
-		System.out.println("I am Neeraj");
 		SpringApplication.run(TestMaven1Application.class, args);
 	}
 
