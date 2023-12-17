05231bd3e32e35deb0dc38d2120cae5830b3106b
Neeraj <neerajiitdh@gmail.com>
2023-12-17 10:50:04 +0530

    First Commit

---
+++ src/main/java/com/neeraj/TestMaven1/TestMaven1Application.java
@@ -0,0 +1,14 @@
+package com.neeraj.TestMaven1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMaven1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+		SpringApplication.run(TestMaven1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neeraj/TestMaven1/TestMaven1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.TestMaven1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMaven1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
